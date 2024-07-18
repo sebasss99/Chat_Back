277bcff15d5fa228581c8c9545d71abced5166f2
Sebastian Matallana <sebastian.matallana99@gmail.com>
2024-07-18 15:58:06 -0500

    First commit

---
+++ src/main/java/com/chat/socket/SmChatSocketApplication.java
@@ -0,0 +1,13 @@
+package com.chat.socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmChatSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmChatSocketApplication.class, args);
+	}
+
+}
